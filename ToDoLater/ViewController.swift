ef8a5ee64cc811b58017018db8e2cf71706c90b7
Aleksandrs Muravjovs <kavalskij@gmail.com>
2018-10-29 10:19:37 +0200

    Initial Commit

---
+++ ToDoLater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoLater
+//
+//  Created by Aleksandrs Muravjovs on 29/10/2018.
+//  Copyright © 2018 Aleksandrs Muravjovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
